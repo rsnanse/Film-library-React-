6707531e6fd57e51cb85806e38bfb50445510496
Niko Petrov <rsnanse@yandex.ru>
2026-04-14 19:30:29 +0400

    ДЗ Контекст(прокидывание кастомного хука авторизации через контекст)

--- film-library/src/App.jsx
+++ film-library/src/App.jsx
@@ -6,7 +6,7 @@ import SearchIconSvg from '/search-icon.svg';
 import CardList from './components/CardList/CardList';
 import Form from './components/Form/Form';
 import { useSearchFilm } from './hooks/Search.hook';
-import { useAuth } from './hooks/Auth.hook';
+import { UserContextProvider } from './Context/UserContext';
 
 const INITIAL_DATA = [
     { id: 1, rating: 1, poster: '/black-widow.jpg', title: 'Black Widow', favorites: 'В избранное' },
@@ -18,17 +18,18 @@ const INITIAL_DATA = [
 
 function App() {
     const { searchQuery, onChangeSearch, handleSearch } = useSearchFilm('');
-    const { userName, activeUser, onChangeProfile, handleProfile, handleLogOut } = useAuth('');
     return (
-        <div className="app">
-            <Background></Background>
-            <NavigationPanel activeUser={activeUser} logOut={handleLogOut}></NavigationPanel>
-            <SearchHeader title={'Поиск'} innerText={'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'}></SearchHeader>
-            <Form onSubmit={handleSearch} value={searchQuery} icon={SearchIconSvg} text={'Искать'} placeholder={'Введите название'} onChange={onChangeSearch} direction={'search-wrapper'}></Form>
-            <CardList films={INITIAL_DATA}></CardList>
-            <SearchHeader title={'Вход'}></SearchHeader>
-            <Form onSubmit={handleProfile} value={userName} text={'Войти в профиль'} placeholder={'Ваше имя'} onChange={onChangeProfile}></Form>
-        </div>
+        <UserContextProvider>
+            <div className="app">
+                <Background></Background>
+                <NavigationPanel></NavigationPanel>
+                <SearchHeader title={'Поиск'} innerText={'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'}></SearchHeader>
+                <Form onSubmit={handleSearch} value={searchQuery} icon={SearchIconSvg} text={'Искать'} placeholder={'Введите название'} onChange={onChangeSearch} direction={'search-wrapper'}></Form>
+                <CardList films={INITIAL_DATA}></CardList>
+                <SearchHeader title={'Вход'}></SearchHeader>
+                <Form text={'Войти в профиль'} placeholder={'Ваше имя'}></Form>
+            </div>
+        </UserContextProvider>
     );
 }
 
--- film-library/src/components/Form/Form.jsx
+++ film-library/src/components/Form/Form.jsx
@@ -1,15 +1,17 @@
+import { useAuth } from '../../hooks/Auth.hook';
 import styles from './Form.module.css';
-function Form({ placeholder, value, onChange, icon, text, onSubmit, direction }) {
+function Form({ placeholder, icon, text, direction }) {
+    const { handleProfile, userName, onChangeProfile } = useAuth();
     return (
-        <form className={styles[direction ? 'search-wrapper' : 'login-wrapper']} onSubmit={onSubmit}>
+        <form className={styles[direction ? 'search-wrapper' : 'login-wrapper']} onSubmit={handleProfile}>
             <div className={styles.wrapper}>
                 {icon ? (
                     <>
-                        <input className={styles.input} type="text" placeholder={placeholder} value={value} onChange={onChange} />
+                        <input className={styles.input} type="text" placeholder={placeholder} value={userName} onChange={onChangeProfile} />
                         <img src={icon} alt="Лупа" className={styles.icon} />
                     </>
                 ) : (
-                    <input className={`${styles.input} ${styles.hidden}`} type="text" placeholder={placeholder} value={value} onChange={onChange} />
+                    <input className={`${styles.input} ${styles.hidden}`} type="text" placeholder={placeholder} value={userName} onChange={onChangeProfile} />
                 )}
             </div>
             <button type="submit" className={styles['btn-search']}>
